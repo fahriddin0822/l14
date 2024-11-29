32f5adb5401a8bdf2b94a6543f96f869992b9537
Fahriddin <mavlonovfahriddin0822@gmail.com>
2024-11-29 10:59:31 +0500

    asd

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -9,19 +9,19 @@ export default function ProductCard({ product, grid }) {
 
   return (
     <div
-      className={`rounded-md bg-gray-100 w-[320px] p-3 shadow-xl h-[470px]  flex flex-col justify-between max-sm:w-[310px] ${
+      className={`rounded-md bg-gray-100 w-[320px] p-3 shadow-xl h-[500px]  flex flex-col justify-between max-sm:w-[310px] ${
         grid && "w-[100%]"
       }`}
     >
       <img
         src={image}
         alt={`product: ${title}`}
-        className={`object-cover w-full h-[200px] rounded-t-md ${
-          grid && "h-[250px]"
+        className={`object-contain w-full h-[200px] rounded-t-md ${
+          grid && "h-[280px]"
         }`}
       />
       <h3 className="text-xl font-semibold my-1">{title}</h3>
-      <p className="text-sm description" maxLines={5}>
+      <p className="text-sm h-[100px] description" maxLines={5}>
         {description}
       </p>
       <div className="flex justify-between gap-2 my-1">
@@ -35,8 +35,8 @@ export default function ProductCard({ product, grid }) {
           count: <span>{rating?.count}</span>
         </p>
       </div>
-      <div className="flex justify-end mt-1">
-        <button className="border rounded-md p-1 bg-gray-200">
+      <div className="flex justify-start mt-1">
+        <button className="rounded-lg p-1 border border-red-900">
           Price: <span> {price} $</span>
         </button>
       </div>
